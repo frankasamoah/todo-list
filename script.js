c1763e6bff0601e38bf2ca1502150de60dbac1ce
Frank Asamoah <frank.asamoah119@gmail.com>
2022-10-12 20:26:18 +0200

    progressing

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 
-const showDate = document.querySelector(".show-date");
+// selecting the form
+const todoForm = document.querySelector("#todo-Modal");
+
+// selecting the input
+const input = document.querySelector("#input");
+
+// selecting the add button
+const addButton = document.querySelector("#add-btn");
+
+// selecting element to display todos
+const displayTodos = document.querySelector('#display-todos');
+
+const ul = document.createElement('ul');
+console.log(ul)
+
+const li = document.createElement('li');
 
 
- showDate.innerHTML = `Date to do was created ${dayjs().format("dddd, DD MMMM YYYY")}`
+
+
+
+
+
+
+
+const todoModal = () => {
+  // adding event listener to prevent the forms behavior of submitting anytime we load page
+    todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(input.value)
+  })
+
+};
+
+addButton.addEventListener("click", todoModal)
+
+
+
+
+// const showDate = document.querySelector(".show-date");
+
+
+
+
+
