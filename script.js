8f82858ab4d9b8f84b5bd900c386fe367b97d3fe
Frank Asamoah <frank.asamoah119@gmail.com>
2022-10-17 18:03:25 +0200

    changing whole outlook of the project

--- script.js
+++ script.js
@@ -1,59 +1,70 @@
+// SELECTORS
 
-// selecting the form
-const todoForm = document.querySelector("#todo-Modal");
-
-// selecting the input
 const input = document.querySelector("#input");
-
-// selecting the add button
 const addButton = document.querySelector("#add-btn");
+const displayTodos = document.querySelector("#display-todos");
+const todoList = document.querySelector(".todo-list");
+const showDate = document.querySelector('.show-date');
+// const checkButton = document.querySelector(".check-btn");
+// const editButton = document.querySelector(".edit-btn");
+// const deleteButton = document.querySelector(".delete-btn");
 
-// selecting element to display todos
-const displayTodos = document.querySelector('#display-todos');
+// EVENT LISTENERS
 
-const incomingList = document.querySelector('.incoming-list')
+addButton.addEventListener("click", addToDo);
 
-
-const deleteButton = document.querySelector('.delete-button')
-
-
-
-
-
-
+todoList.addEventListener("click", actionsOnToDo)
 
 
 
-const addToDo = () => {
-  // adding event listener to prevent the forms behavior of submitting anytime we load page
-    todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const li = document.createElement('li');
-    li.className = 'add-list'
-    
-    li.textContent = input.value;
+// FUNCTIONS
 
-    incomingList.appendChild(li);
+function addToDo(e) {
+  // prevent the forms behavior of submitting anytime we load page
+  e.preventDefault();
 
-    input.value = ''
-    // console.log(li);
-    // console.log(input.value)
-  })
+  // div to contain todo
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
 
-};
+  // check button
+  const checkButton = document.createElement("button");
+  checkButton.setAttribute('type', 'checkbox');
+  checkButton.classList.add("check-btn");
+  todoDiv.appendChild(checkButton);
 
-addButton.addEventListener("click", addToDo)
+  // list item
+  const todoItem = document.createElement("li");
+  todoItem.innerText = input.value;
+  todoItem.classList.add("todo-item");
+  todoDiv.appendChild(todoItem);
 
-const deleteToDo = () => {
-  incomingList.removeChild(incomingList.firstElementChild)
+  // edit button
+  const editButton = document.createElement("button");
+
+  editButton.innerHTML = `<i class="fas fa-pencil"></i>`;
+
+  editButton.classList.add("edit-btn");
+  todoDiv.appendChild(editButton);
+
+  // delete button
+  const deleteButton = document.createElement("button");
+
+  deleteButton.innerHTML = `<i class="fas fa-cancel"></i>`;
+
+  deleteButton.classList.add("delete-btn");
+  todoDiv.appendChild(deleteButton);
+
+  todoList.appendChild(todoDiv);
+
+
+
+  input.value = "";
 }
 
-deleteButton.addEventListener('click', deleteToDo)
+function actionsOnToDo(e) {
+  const item = e.target;
+  console.log(item);
+}
 
-// const showDate = document.querySelector(".show-date");
-
-
-// const newInput = document.createElement('input');
-// newInput.type = 'checkbox'
-// console.log(newInput.type)
 
