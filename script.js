747d2d0e4d23d0293ec8d75ee2bde35cd993d5d7
Frank Asamoah <frank.asamoah119@gmail.com>
2022-10-18 08:39:11 +0200

    edit button implemented

--- script.js
+++ script.js
@@ -14,37 +14,7 @@ const progressDone = document.querySelector('.progress-done');
 const minInput = document.querySelector('.min-input');
 const maxInput= document.querySelector('.max-input');
 
-let finalValue = 0;
-let max = 0;
 
-function changeWidth() {
-  // want to find what percentage is the progree done to the total todos
-  // what percentage is the progress done to the progress
-
-  progress.style.width = `${
-    (finalValue/max) * 100
-  }%`
-  progress.innerHTML = `${Math.ceil((finalValue/max) * 100
-  )}%`
-}
-
-minInput.addEventListener('keyup', function() {
-  // we set the values from the input to integers so that we can 
-  // work the percentages ...radix of 10
-  finalValue = parseInt(minInput.value, 10);
-
-  // call the changeWidth function here
-//  console.log(finalValue);
-changeWidth()
-});
-
-maxInput.addEventListener('keyup', function() {
-  // we set the values from the input to integers so that we can 
-  // work the percentages ...radix of 10
-  max = parseInt(maxInput.value, 10);
-  // console.log(max);
-  changeWidth()
-})
 
 // EVENT LISTENERS
 
@@ -52,7 +22,8 @@ addButton.addEventListener("click", addToDo);
 
 todoList.addEventListener("click", actionsOnToDo)
 
-// console.log(todoList);
+// add event listener to check if DOM content is loaded tthen run todos on localStorage
+document.addEventListener('DOMContentLoaded', getTodos)
 
 
 
@@ -123,16 +94,24 @@ function actionsOnToDo(e) {
   };
 
   if(item.classList[0] === 'edit-btn') {
-    const newInputDiv = document.createElement('div');
-    newInputDiv.classList.add('new-input_div')
 
+   // new input for editing
     const newIput = document.createElement('input');
     input.classList.add('new-input');
+
     displayTodos.classList.toggle('column-display');
     displayTodos.prepend(newIput);
+
+    // new button to add edited value
+    const newInputButton = document.createElement("button");
+    newInputButton.classList.add('new-input-button');
+    newInputButton.innerHTML = `<i class="fas fa-plus-square"></i>`;
+    displayTodos.appendChild(newInputButton)
+    
     const todoItem = document.querySelector('.todo-item');
      const todoInnerText = todoItem.innerText;
     newIput.value = todoInnerText;
+    todoList.removeChild(todoItem.parentElement)
 
     function editInput(e) {
       todoItem.textContent = e.target.value;
@@ -167,7 +146,81 @@ function saveToLocalSorage(todo) {
    localStorage.setItem('storage', JSON.stringify(storage));
 }
 
+// display todos on localStorage
+function getTodos() {
+  let storage = JSON.parse(localStorage.getItem('storage')) || [];
+ 
+  //todos in the storage are looped over
+  storage.forEach(function(todo) {
+  // then the add todo function is recreated
 
-function progressBar() {
+  const todoDiv = document.createElement("div");
+todoDiv.classList.add("todo");
 
+// check button
+const checkButton = document.createElement("button");
+checkButton.innerHTML = `<i class="fas fa-square-check"></i>`;
+checkButton.classList.add("check-btn");
+todoDiv.appendChild(checkButton);
+
+// list item
+const todoItem = document.createElement("li");
+todoItem.innerText = todo;
+todoItem.classList.add("todo-item");
+todoDiv.appendChild(todoItem);
+
+
+// edit button
+const editButton = document.createElement("button");
+
+editButton.innerHTML = `<i class="fas fa-pencil"></i>`;
+
+editButton.classList.add("edit-btn");
+todoDiv.appendChild(editButton);
+
+// delete button
+const deleteButton = document.createElement("button");
+
+deleteButton.innerHTML = `<i class="fas fa-xmark"></i>`;
+
+deleteButton.classList.add("delete-btn");
+todoDiv.appendChild(deleteButton);
+
+todoList.appendChild(todoDiv);
+  })
 }
+
+
+
+// progressbar
+let finalValue = 0;
+let max = 0;
+
+function changeWidth() {
+  // want to find what percentage is the progree done to the total todos
+  // what percentage is the progress done to the progress
+
+  progress.style.width = `${
+    (finalValue/max) * 100
+  }%`
+  progress.innerHTML = `${Math.ceil((finalValue/max) * 100
+  )}%`
+}
+
+minInput.addEventListener('keyup', function() {
+  // we set the values from the input to integers so that we can 
+  // work the percentages ...radix of 10
+  finalValue = parseInt(minInput.value, 10);
+
+  // call the changeWidth function here
+//  console.log(finalValue);
+changeWidth()
+});
+
+maxInput.addEventListener('keyup', function() {
+  // we set the values from the input to integers so that we can 
+  // work the percentages ...radix of 10
+  max = parseInt(maxInput.value, 10);
+  // console.log(max);
+  changeWidth()
+})
